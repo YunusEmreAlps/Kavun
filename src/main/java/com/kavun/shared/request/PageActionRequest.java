42343c9587ec00fe23965c211da507c307ef1ab2
Yunus Emre Alpu <yunus192alpu@gmail.com>
2026-03-29 20:44:43 +0300

    refactor: Update PageAction and related DTOs to improve null handling and validation

--- src/main/java/com/kavun/shared/request/PageActionRequest.java
+++ src/main/java/com/kavun/shared/request/PageActionRequest.java
@@ -1,6 +1,7 @@
 package com.kavun.shared.request;
 
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -8,9 +9,15 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class PageActionRequest extends BaseRequest {
+    @NotNull(message = "Page ID cannot be null")
     private Long pageId;
+
+    @NotNull(message = "Action ID cannot be null")
     private Long actionId;
-    private String apiEndpoint;
-    private String httpMethod;
+
+    private String apiEndpoint;  // Optional
+    private String httpMethod;    // Optional
+
+    @NotBlank(message = "Label cannot be blank")
     private String label;
 }
